fff274e0dec4f10dbb7ec913fbea603655363644
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2022-09-08 14:32:53 -0700

    Add unit test project.

---
+++ test/LaunchDarkly.OpenFeature.ServerProvider.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace LaunchDarkly.OpenFeature.ServerProvider.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
