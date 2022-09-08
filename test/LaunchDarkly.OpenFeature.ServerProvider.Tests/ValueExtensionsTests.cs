896acc1bb0421be149dd5a8991febf7bc9a24e17
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2022-09-08 15:17:05 -0700

    Add a test  for lists.

--- test/LaunchDarkly.OpenFeature.ServerProvider.Tests/ValueExtensionsTests.cs
+++ test/LaunchDarkly.OpenFeature.ServerProvider.Tests/ValueExtensionsTests.cs
@@ -31,5 +31,38 @@ namespace LaunchDarkly.OpenFeature.ServerProvider.Tests
                 Assert.Equal(expectedValue.AsString, ldValue.AsString);
             });
         }
+
+        [Fact]
+        public void ItCanConvertArrays()
+        {
+            var ofValueList = new Value(new List<Value>
+            {
+                new Value(true),
+                new Value(false),
+                new Value(17),
+                new Value(17.5),
+                new Value("string")
+            });
+            
+            ofValueList.Extract(ldValue =>
+            {
+                var listFromValue = ldValue.List;
+                Assert.NotNull(listFromValue);
+                Assert.Equal(LdValueType.Bool, listFromValue[0].Type);
+                Assert.True(listFromValue[0].AsBool);
+                
+                Assert.Equal(LdValueType.Bool, listFromValue[1].Type);
+                Assert.False(listFromValue[1].AsBool);
+                
+                Assert.Equal(LdValueType.Number, listFromValue[2].Type);
+                Assert.Equal(17, listFromValue[2].AsInt);
+                
+                Assert.Equal(LdValueType.Number, listFromValue[3].Type);
+                Assert.Equal(17.5, listFromValue[3].AsDouble);
+                
+                Assert.Equal(LdValueType.String, listFromValue[4].Type);
+                Assert.Equal("string", listFromValue[4].AsString);
+            });
+        }
     }
 }
