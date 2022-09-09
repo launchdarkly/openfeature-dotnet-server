b3bc864c03ed7adb7f2066748c6cc8899566c2a1
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2022-09-09 09:59:24 -0700

    Remove unused using statements.

--- src/LaunchDarkly.OpenFeature.ServerProvider/EvalContextExtensions.cs
+++ src/LaunchDarkly.OpenFeature.ServerProvider/EvalContextExtensions.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using LaunchDarkly.Sdk;
 using OpenFeature.SDK.Model;
 
--- src/LaunchDarkly.OpenFeature.ServerProvider/LdValueExtensions.cs
+++ src/LaunchDarkly.OpenFeature.ServerProvider/LdValueExtensions.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using LaunchDarkly.Sdk;
 using OpenFeature.SDK.Model;
