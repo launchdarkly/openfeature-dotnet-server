1283bdfba4ffa3b2a24d8db7424ca9ac557d7de2
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2022-09-13 09:12:37 -0700

    Update to use expression bodies.

--- src/LaunchDarkly.OpenFeature.ServerProvider/Provider.cs
+++ src/LaunchDarkly.OpenFeature.ServerProvider/Provider.cs
@@ -21,39 +21,29 @@ namespace LaunchDarkly.OpenFeature.ServerProvider
         public override Metadata GetMetadata() => _metadata;
 
         public override Task<ResolutionDetails<bool>> ResolveBooleanValue(string flagKey, bool defaultValue,
-            EvaluationContext context = null)
-        {
-            return Task.FromResult(_client.BoolVariationDetail(flagKey, context.ToLdUser(), defaultValue)
-                .ToResolutionDetails(flagKey));
-        }
+            EvaluationContext context = null) => Task.FromResult(_client
+            .BoolVariationDetail(flagKey, context.ToLdUser(), defaultValue)
+            .ToResolutionDetails(flagKey));
 
         public override Task<ResolutionDetails<string>> ResolveStringValue(string flagKey, string defaultValue,
-            EvaluationContext context = null)
-        {
-            return Task.FromResult(_client.StringVariationDetail(flagKey, context.ToLdUser(), defaultValue)
-                .ToResolutionDetails(flagKey));
-        }
+            EvaluationContext context = null) => Task.FromResult(_client
+            .StringVariationDetail(flagKey, context.ToLdUser(), defaultValue)
+            .ToResolutionDetails(flagKey));
 
         public override Task<ResolutionDetails<int>> ResolveIntegerValue(string flagKey, int defaultValue,
-            EvaluationContext context = null)
-        {
-            return Task.FromResult(_client.IntVariationDetail(flagKey, context.ToLdUser(), defaultValue)
-                .ToResolutionDetails(flagKey));
-        }
+            EvaluationContext context = null) => Task.FromResult(_client
+            .IntVariationDetail(flagKey, context.ToLdUser(), defaultValue)
+            .ToResolutionDetails(flagKey));
 
         public override Task<ResolutionDetails<double>> ResolveDoubleValue(string flagKey, double defaultValue,
-            EvaluationContext context = null)
-        {
-            return Task.FromResult(_client.DoubleVariationDetail(flagKey, context.ToLdUser(), defaultValue)
-                .ToResolutionDetails(flagKey));
-        }
+            EvaluationContext context = null) => Task.FromResult(_client
+            .DoubleVariationDetail(flagKey, context.ToLdUser(), defaultValue)
+            .ToResolutionDetails(flagKey));
 
         public override Task<ResolutionDetails<Value>> ResolveStructureValue(string flagKey, Value defaultValue,
-            EvaluationContext context = null)
-        {
-            return Task.FromResult(_client.JsonVariationDetail(flagKey, context.ToLdUser(), LdValue.Null)
-                .ToValueDetail(defaultValue).ToResolutionDetails(flagKey));
-        }
+            EvaluationContext context = null) => Task.FromResult(_client
+            .JsonVariationDetail(flagKey, context.ToLdUser(), LdValue.Null)
+            .ToValueDetail(defaultValue).ToResolutionDetails(flagKey));
 
         #endregion
     }
