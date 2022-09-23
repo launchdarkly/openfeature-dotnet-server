5e1ebfdd657a25544bd637131cb2231aa892f0a0
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2022-09-23 08:52:58 -0700

    Add releaser config. Remove AssemblyInfo and move InternalsVisibileTo to the csproj. (#5)

--- src/LaunchDarkly.OpenFeature.ServerProvider/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-using System.Runtime.CompilerServices;
-
-#if DEBUG
-// Allow unit tests to see internal classes (note, the test assembly is not signed;
-// tests must be run against the Debug configuration of this assembly)
-[assembly: InternalsVisibleTo("LaunchDarkly.OpenFeature.ServerProvider.Tests")]
-
-#endif
